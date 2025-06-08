2441ab50f8eebf6ad1e4c0a3c4e656664e487360
vernellm <vtmangum@gmail.com>
2025-06-08 16:52:45 -0500

    added first name

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Author: Vernell Mangum
+# Welcome to the first step of a web developer going through his Python journey! :)
+
+print("V   V   EEEEE   RRRR    N   N   EEEEE   L       L    ")
+print("V   V   E       R   R   NN  N   E       L       L    ")
+print("V   V   E       R   R   N N N   E       L       L    ")
+print("V   V   EEE     RRRR    N  NN   EEE     L       L    ")
+print("V   V   E       R R     N   N   E       L       L    ")
+print(" V V    E       R  R    N   N   E       L       L    ")
+print("  V     EEEEE   R   R   N   N   EEEEE   LLLLL   LLLLL") 
